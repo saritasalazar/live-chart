4c9885bc1f223025d8bcb153465ca1e800bedde2
Sara <saraesalazar@icloud.com>
2021-05-07 00:01:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import { Subscription } from 'react-apollo';
+import gql from 'graphql-tag';
+import moment from 'moment';
+
+const TWENTY_MIN_TEMP_SUB= gql`
+  subscription {
+    last_20_min_temp(
+      order_by: {
+        five_sec_interval: asc
+      }
+      where: {
+        location: {
+          _eq: "Miami"
+        }
+      }
+    ) {
+      five_sec_interval
+      location
+      max_temp
+    }
+  }
+`
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div
+        style={{display: 'flex', alignItems: 'center', justifyContent: 'center', margin: '20px'}}
+      >
+    <Subscription subscription={TWENTY_MIN_TEMP_SUB}>
+          {
+            ({data, error, loading}) => {
+              if (error) {
+                console.error(error);
+                return "Error";
+              }
+              if (loading) {
+                return "Loading";
+              }
+              let chartJSData = {
+                labels: [],
+                datasets: [{
+                  label: "Max temp every 5 secs",
+                  data: [],
+                  pointBackgroundColor: [],
+                  borderColor: 'lavendar',
+                  fill: false
+                }]
+              };
+              data.last_20_min_temp.forEach((item) => {
+                const formattedTime = moment(item.five_sec_interval).format('LTS');
+                chartJSData.labels.push(formattedTime);
+                chartJSData.datasets[0].data.push(item.max_temp);
+                chartJSData.datasets[0].pointBackgroundColor.push('aqua');
+              })
+              return (
+                <Line
+                  data={chartJSData}
+                  options={{
+                    animation: {duration: 0},
+                    scales: { yAxes: [{ticks: { min: 5, max: 20 }}]}
+                  }}
+                />
+              );
+            }
+          }
+        </Subscription>
+      </div>
+    );
+  }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { WebSocketLink } from 'apollo-link-ws';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const link = new WebSocketLink({
+  uri: `${process.env.REACT_APP_HASURA_URI}`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      headers: {
+        "x-hasura-admin-secret":`${process.env.REACT_APP_HASURA_PASS}`
+      }
+    }
+  }
+})
+const cache = new InMemoryCache();
+const client = new ApolloClient({
+  link,
+  cache
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+   <ApolloProvider client={client}> 
+      <App />
+    </ApolloProvider>,
   document.getElementById('root')
 );
 
